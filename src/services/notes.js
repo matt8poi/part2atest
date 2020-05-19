12aa699d0428e789e673c9be5af7919b913e502e
matt8poi <matt8poi@gmail.com>
2020-05-18 22:02:51 -0600

    Finished the notes appfor part 2.

--- src/services/notes.js
+++ src/services/notes.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
-//   const request = axios.get(baseUrl)
-//   return request.then(response => response.data)
     const request = axios.get(baseUrl)
     const nonExisting = {
     id: 10000,
